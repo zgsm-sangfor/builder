d5b95925afae2ba184de7b63d7906aed151dd3a0
dengbinbox <dengbinbox@gmail.com>
2026-07-06 20:05:40 +0800

    feat: 修改静态文件路由

--- configures/common/firmware/apisix-costrict-apps.sh
+++ configures/common/firmware/apisix-costrict-apps.sh
@@ -29,18 +29,12 @@ curl -i http://$APISIX_ADDR/apisix/admin/routes -H "$AUTH" -H "$TYPE" -X PUT -d 
     }
   }'
 
+# 私有化不设置限制
 curl -i http://$APISIX_ADDR/apisix/admin/routes -H "$AUTH" -H "$TYPE" -X PUT -d '{
-    "uri": "/costrict/*",
+    "uris": ["/costrict/*","/costrict-static/*"],
     "id": "costrict-apps",
     "name": "costrict-apps",
     "upstream_id": "costrict-apps",
     "plugins": {
-      "limit-count": {
-        "count": 300,
-        "time_window": 60,
-        "rejected_code": 429,
-        "key_type": "var_combination",
-        "key": "$remote_addr $http_x_forwarded_for"
-      }
     }
   }'
